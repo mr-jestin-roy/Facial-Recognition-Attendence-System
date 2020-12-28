25d62c73474aca64d5fe3611a7b4254837fb0c93
mr-jestin-roy <roy_jestin@yahoo.com>
2020-12-28 11:34:20 +0530

    Addition of new users with Date Time

--- Basic.py
+++ Basic.py
@@ -2,9 +2,9 @@ import face_recognition
 import cv2
 import numpy as np
 
-imgElon = face_recognition.load_image_file('ImagesBasic/Elon Musk.jpg')
+imgElon = face_recognition.load_image_file('ImagesBasic/Jestin Roy.jpg')
 imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
-imgTest = face_recognition.load_image_file('ImagesBasic/Jestin Roy.jpg')
+imgTest = face_recognition.load_image_file('ImagesBasic/Jestin_test.jpg')
 imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
 faceLoc = face_recognition.face_locations(imgElon)[0]
@@ -19,6 +19,6 @@ results = face_recognition.compare_faces([encodeElon], encodeTest)
 faceDis = face_recognition.face_distance([encodeElon], encodeTest)
 cv2.putText(imgTest,f'{results} {round(faceDis[0],2)} ',(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,255),3)
 
-cv2.imshow('Elon Musk',imgElon)
-cv2.imshow('Elon Test',imgTest)
+cv2.imshow('DataBase Image',imgElon)
+cv2.imshow('Actual Image',imgTest)
 cv2.waitKey(0)
